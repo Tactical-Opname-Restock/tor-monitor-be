8b44dd6a05f76fea7b4652010b6907ba5e1537cd
Ahmad Ghozali <ahmadgozali4040@gmail.com>
2025-12-04 13:42:12 +0700

    feat[route]: add dashboard route

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from os import environ
 
-from .routers import auth, goods, sales
+from .routers import auth, goods, sales, dashboard
 
 load_dotenv()
 logger = logging.getLogger(__name__)
@@ -16,13 +16,14 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:8081" if environ.get("DEV_KEY") == "dev" else "*"
-    ],  # Allow only this origin
+        "*" if environ.get("DEV_KEY") == "dev" else environ.get("FRONTEND_URL")
+    ],
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
 )
 
 app.include_router(auth.router)
+app.include_router(dashboard.router)
 app.include_router(goods.router)
 app.include_router(sales.router)
